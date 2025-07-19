b20e926b979f93c9acf7cf14b3cac002365874c2
Rui Peng <1020314683@qq.com>
2025-07-19 11:09:22 +0800

    init commit
    第一次提交，基于Ruoyi Sqlite封装的可执行程序

---
+++ preload.js
@@ -0,0 +1,60 @@
+/**
+ * The preload script runs before `index.html` is loaded
+ * in the renderer. It has access to web APIs as well as
+ * Electron's renderer process modules and some polyfilled
+ * Node.js functions.
+ *
+ * https://www.electronjs.org/docs/latest/tutorial/sandbox
+ */
+
+const { contextBridge } = require('electron')
+const { spawn, exec } = require('child_process')
+const path = require('path')
+
+contextBridge.exposeInMainWorld('myAPI', {
+  startServerForSpawn: () => {
+    console.log('startServerForSpawn')
+    let path1 = path.join(__dirname, 'app/ruoyi-admin.jar');
+    const ls = spawn('java', ['-jar', path1]);
+    ls.stdout.on('data', (data) => {
+      if(data.toString().indexOf("Started RuoYiApplication") !== -1){
+        window.location.href="http://localhost:80";
+      }
+    });
+    ls.stderr.on('data', (data) => {
+      console.error('stderr: ${data}');
+      alert("启动服务异常");
+    });
+    ls.on('close', (code) => {
+      console.log('child process exited with code ${code}');
+    });
+  },
+  startServerForbat: () =>{
+    console.log('startServerForbat')
+    const bat = spawn(path.join(__dirname, 'my.bat'));
+    bat.stdout.on('data', (data) => {
+      console.log(data.toString());
+      if(data.toString().indexOf("Started RuoYiApplication") !== -1){
+        window.location.href="http://localhost:80";
+      }
+    });
+    bat.stderr.on('data', (data) => {
+      console.error(data.toString());
+    });
+    bat.on('exit', (code) => {
+      console.log('Child exited with code ${code}');
+    });
+  }
+})
+
+
+// window.addEventListener('DOMContentLoaded', () => {
+//   const replaceText = (selector, text) => {
+//     const element = document.getElementById(selector)
+//     if (element) element.innerText = text
+//   }
+
+//   for (const type of ['chrome', 'node', 'electron']) {
+//     replaceText(`${type}-version`, process.versions[type])
+//   }
+// })
